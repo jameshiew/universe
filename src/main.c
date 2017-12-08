798d0f20049771f589c660b26d5b76985d6e448a
James Hiew <james@hiew.net>
2017-12-08 23:01:28 +0000

    Initialize the VBO with some triangle vertices

--- src/main.c
+++ src/main.c
@@ -36,6 +36,18 @@ int main() {
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        float vertices[] = {
+                -0.5f, -0.5f, 0.0f,
+                0.5f, -0.5f, 0.0f,
+                0.0f,  0.5f, 0.0f
+        };
+
+        GLuint VBO;
+        glGenBuffers(1, &VBO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
