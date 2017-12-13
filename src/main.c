c41db6d7acb1c8fda0ea3c03af564b6c011785f8
James Hiew <james@hiew.net>
2017-12-13 02:24:48 +0000

    modelviewprojection matrices

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include "shaders.h"
 #include "render.h"
 #include "polygon.h"
+#include "config.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -28,7 +29,18 @@ int main() {
     polygon *p = polygon_new();
     GLuint texture = load_texture("../../textures/container.jpg");
 
+    mat4_t model = m4_rotation_x(-1.0f);
+    mat4_t view = m4_translation(vec3(0.0f, 0.0f, -3.0f));
+    mat4_t projection = m4_perspective(45.0f, DEFAULT_WINDOW_WIDTH / DEFAULT_WINDOW_HEIGHT, 0.1f, 100.0f);
+
     glUseProgram(shaderProgram);
+    GLint transformLoc = glGetUniformLocation(shaderProgram, "model");
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, (const GLfloat *)&model);
+    transformLoc = glGetUniformLocation(shaderProgram, "view");
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, (const GLfloat *)&view);
+    transformLoc = glGetUniformLocation(shaderProgram, "projection");
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, (const GLfloat *)&projection);
+
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
@@ -40,11 +52,6 @@ int main() {
         double greenValue = (sin(timeValue) / 2.0f) + 0.5f;
         GLint vertexColorLocation = glGetUniformLocation(shaderProgram, "baseColor");
         glUniform4f(vertexColorLocation, 0.0f, (GLfloat) greenValue, 0.0f, 1.0f);
-
-        mat4_t trans = m4_rotation_z(sin(timeValue));
-
-        GLint transformLoc = glGetUniformLocation(shaderProgram, "transform");
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, (const GLfloat *)&trans);
 
         // seeing as we only have a single VAO there's no need to bind it every time,
         // but we'll do so to keep things a bit more organized
