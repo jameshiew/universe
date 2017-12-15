9c6de52fb621d0aaca8d1a659d24c3cff4daa68e
James Hiew <james@hiew.net>
2017-12-15 02:25:27 +0000

    Remove unused

--- src/shaders.h
+++ src/shaders.h
@@ -5,7 +5,6 @@
 #ifndef UNIVERSE_SHADERS_H
 #define UNIVERSE_SHADERS_H
 
-GLuint get_shader_program();
 GLuint make_shader(GLenum type, const char *source);
 GLuint load_shader(GLenum type, const char *path);
 GLuint load_program(const char *vertexShaderPath, const char *fragmentShaderPath);
