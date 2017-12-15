efd9c4eb51edb165b0c615fe5a1b7bdce02a8e61
James Hiew <james@hiew.net>
2017-12-15 06:35:11 +0000

    constantify some directions

---
+++ src/space.cpp
@@ -0,0 +1,12 @@
+//
+// Created by James Hiew on 15/12/2017.
+//
+
+#include "space.hpp"
+
+glm::vec3 UP = glm::vec3(0.0f, 1.0f, 0.0f);
+glm::vec3 DOWN = -UP;
+glm::vec3 NORTH = glm::vec3(1.0f, 0.0f, 0.0f);
+glm::vec3 WEST = glm::vec3(0.0f, 0.0f, 1.0f);
+glm::vec3 EAST = -WEST;
+glm::vec3 SOUTH = -NORTH;
