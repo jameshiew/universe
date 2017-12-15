3b871f1eb294fb9b0e3b87ccf77cb3e5dc140a1a
James Hiew <james@hiew.net>
2017-12-15 07:15:06 +0000

    Tidy

--- src/shaders.cpp
+++ src/shaders.cpp
@@ -2,9 +2,11 @@
 // Created by James Hiew on 09/12/2017.
 //
 
+#include "render.hpp"
 #include <glad/glad.h>
 #include <cstdio>
 #include <cstdlib>
+#include <stb_image.h>
 
 #include "shaders.hpp"
 #include "util.hpp"
@@ -60,4 +62,27 @@ GLuint make_program(GLuint vertexShader, GLuint fragmentShader) {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
     return program;
+}
+
+GLuint load_texture(const char *texturePath) {
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load(texturePath, &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        fputs("Failed to load texture", stderr);
+    }
+    stbi_image_free(data);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    return texture;
 }
--- src/shaders.hpp
+++ src/shaders.hpp
@@ -10,4 +10,6 @@ GLuint load_shader(GLenum type, const char *path);
 GLuint load_program(const char *vertexShaderPath, const char *fragmentShaderPath);
 GLuint make_program(GLuint vertexShader, GLuint fragmentShader);
 
+GLuint load_texture(const char* texturePath);
+
 #endif //UNIVERSE_SHADERS_H
