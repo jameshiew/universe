f5e5d2ec76499a06aba9663b10c383b07f8f77cb
James Hiew <james@hiew.net>
2017-12-17 09:27:52 +0000

    ?

--- src/space.cpp
+++ src/space.cpp
@@ -2,6 +2,7 @@
 // Created by James Hiew on 15/12/2017.
 //
 
+#include <random>
 #include "space.hpp"
 #include "polygon.hpp"
 
@@ -22,8 +23,29 @@ void Chunk_free(Chunk *chunk) {
     free(chunk);
 }
 
+void *World::generate(World *world, int x, int y, int z) {
+    world->chunks[x][y][z] = Chunk_new();
+    std::default_random_engine prng;
+    std::uniform_real_distribution<float> distribution(0.f, 100.f);
+    for (int _x = 0; _x < 32; _x++) {
+        for (int _y = -1; _y < 32; _y++) {
+            for (int _z = -1; _z < 32; _z++) {
+                world->chunks[x][y][z]->blocks[_x][_y][_z] = reinterpret_cast<bool *>(distribution(prng) > 90.f);
+            }
+        }
+    }
+}
+
 World *World_new() {
-    return (World *)malloc(sizeof(World));
+    auto world = (World *)malloc(sizeof(World));
+    // pregenerate origin
+    for (int x = -1; x < 2; x++) {
+        for (int y = -1; y < 2; y++) {
+            for (int z = -1; z < 2; z++) {
+                world->generate(world, x, y, z);
+            }
+        }
+    }
 }
 
 void World_free(World *world) {
@@ -44,10 +66,73 @@ std::list<DrawInstruction *> *World_get_draw_instructions(World *world, glm::vec
             for (int z = -1; z < 2; z++) {
                 auto chunk = world->chunks[x][y][z];
                 auto origin = glm::vec3(x * 32.f, y * 32.f, z * 32.f);
-                drawInstructions->push_back(test(origin));
+
+                auto draw = (DrawInstruction *)malloc(sizeof(DrawInstruction));
+                glGenVertexArrays(1, &(draw->vao));
+                glBindVertexArray(draw->vao);
+
+                draw->vertexSize = 8 * sizeof(float);
+                draw->vertexCount = 24;
+                draw->mode = GL_TRIANGLE_STRIP;
+
+                float CUBE_STRIP_VERTICES[] = {
+                        // south          // normal         // texture
+                        0.0f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+                        1.0f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+                        0.0f, 1.0f, 1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+                        1.0f, 1.0f, 1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+                        // east
+                        1.0f, 0.0f, 1.0f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f,
+                        1.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f,
+                        1.0f, 1.0f, 1.0f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f,
+                        1.0f, 1.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
+                        // north
+                        1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+                        0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+                        1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+                        0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+                        // west
+                        0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+                        0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+                        0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f,
+                        0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+                        // down
+                        0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f, 1.0f,
+                        1.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f,
+                        0.0f, 0.0f, 1.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+                        1.0f, 0.0f, 1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+                        // up
+                        0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+                        1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+                        0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+                        1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f,
+                };
+
+                glGenBuffers(1, &(draw->vbo));
+                glBindBuffer(GL_ARRAY_BUFFER, draw->vbo);
+                glBufferData(GL_ARRAY_BUFFER, sizeof(CUBE_STRIP_VERTICES), CUBE_STRIP_VERTICES, GL_STATIC_DRAW);
+
+                // position attribute
+                glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, draw->vertexSize, nullptr);
+                glEnableVertexAttribArray(0);
+                // normal attribute
+                glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, draw->vertexSize, (void *) (3 * sizeof(float)));
+                glEnableVertexAttribArray(1);
+                // uv attribute
+                glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, draw->vertexSize, (void *) (6 * sizeof(float)));
+                glEnableVertexAttribArray(2);
+
+                draw->count = 32;
+                draw->positions = (glm::vec3 *)(malloc(sizeof(glm::vec3) * draw->count));
+                std::default_random_engine prng;
+                std::uniform_real_distribution<float> distribution(-100.f,100.f);
+                for (int i = 0; i < draw->count; i++) {
+                    draw->positions[i] = glm::vec3(distribution(prng), distribution(prng), distribution(prng));
+                    draw->positions[i] += origin;
+                }
+                drawInstructions->push_back(draw);
             }
         }
     }
     return drawInstructions;
 }
-
--- src/space.hpp
+++ src/space.hpp
@@ -19,13 +19,14 @@ extern const glm::vec3 SOUTH;
 extern const glm::mat4 IDENTITY;
 
 typedef struct Chunk {
-    char *blocks[32][32][32];
+    bool *blocks[32][32][32];
 } Chunk;
 
 Chunk *Chunk_new();
 void Chunk_free(Chunk *chunk);
 
 typedef struct World {
+    void *generate(World*, int, int, int);
     Chunk *chunks[16][16][16];
 } World;
 
